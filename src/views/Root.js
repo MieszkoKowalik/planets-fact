8bee7c961b352722f032d43bb17a792046c7f78d
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-10 21:09:25 +0100

    add Navigate element in routes to redirect from main page

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "assets/styles/globalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "assets/styles/theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainTemplate from "components/templates/MainTemplate";
 import PlanetsSection from "components/organisms/PlanetsSection/PlanetsSection";
 import PlanetsProvider from "providers/PlanetsProviders";
@@ -13,6 +13,10 @@ function Root() {
         <PlanetsProvider>
           <MainTemplate>
             <Routes>
+              <Route
+                path="/"
+                element={<Navigate to="mercury/overview" />}
+              ></Route>
               <Route path=":planet/*" element={<PlanetsSection />}>
                 <Route path=":id" element={<PlanetsSection />}></Route>
               </Route>
