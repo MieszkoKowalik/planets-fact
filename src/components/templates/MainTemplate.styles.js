c0c8c8380735a5e4c7c162638981d3df1fd8f4e5
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-10 14:25:53 +0100

    add backgeound stars

--- src/components/templates/MainTemplate.styles.js
+++ src/components/templates/MainTemplate.styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import bgStars from "assets/images/background-stars.svg";
 
 export const Wrapper = styled.div`
   min-height: 100vh;
   background-color: ${({ theme }) => theme.colors.primary};
+  background-image: url(${bgStars});
+  background-position: center;
 `;
